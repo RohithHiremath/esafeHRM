3aaee4ccca39530a0daef283973a6de8c50b8ac8
RohithHiremath <rohithhiremath4624@gmail.com>
2019-10-18 15:36:10 +0530

    Added Logo Responsive datatables and error message for login page

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.views.decorators.csrf import csrf_protect
 # Create your views here.
  
@@ -18,6 +19,7 @@ def login(request):
             # Redirect to Dashboard.
             return  redirect('dashboard')
         else:
+            messages.error(request,'Invalid Username or Password')
             return redirect('login')
     else:
         return redirect('/')
