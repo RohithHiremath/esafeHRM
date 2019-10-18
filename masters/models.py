a31ffd7eeb13bfc82a87e6eec05ee342ea11f865
RohithHiremath <rohithhiremath4624@gmail.com>
2019-10-18 15:58:13 +0530

    pulling updates

--- masters/models.py
+++ masters/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Job(models.Model):
     jobtitle = models.CharField(max_length=50)
     jobdescription = models.CharField(max_length=100)
-    status = models.SmallIntegerField(default=1)
+    # status = models.SmallIntegerField(default=1)
     
     def __str__(self):
        return self.jobtitle
